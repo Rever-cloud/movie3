7e24fc123006e5b44935c8b12c26d22d1904960c
Rever-cloud <dollartrader06@gmail.com>
2026-06-01 13:14:51 +0300

    Update next.config.js

--- movie-streaming-platform/next.config.js
+++ movie-streaming-platform/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'streamflex-movies-api.up.railway.app',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
         pathname: '/**',
       },
     ],
